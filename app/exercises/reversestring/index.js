ec7d6304de3b76ebf09ca68c85e1f77d26cd808d
marcell <ciszekmarcell@gmail.com>
2019-04-11 15:42:50 +0200

    first commit

--- app/exercises/reversestring/index.js
+++ app/exercises/reversestring/index.js
@@ -6,6 +6,28 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  let reverseStr = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverseStr += str[i];
+  }
+  return reverseStr;
+}
+function reverse2(str) {
+  return str
+    .split('')
+    .reverse()
+    .join('');
+}
+function reverse3(str) {
+  let reversedStr = '';
+  for (const char of str) {
+    reversedStr = char + reversedStr;
+  }
+  return reversedStr;
+}
+function reverse4(str) {
+  return str.split('').reduce((reversed, char) => char + reversed, '');
+}
 
 module.exports = reverse;
