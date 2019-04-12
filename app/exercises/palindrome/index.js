40715c5f0820a3ff9cd3a19a8abf11a8240d116c
marcell <ciszekmarcell@gmail.com>
2019-04-12 09:20:51 +0200

    palindrome

--- app/exercises/palindrome/index.js
+++ app/exercises/palindrome/index.js
@@ -7,6 +7,32 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  const reversed = str
+    .split('')
+    .reverse()
+    .join('');
+  return reversed === str;
+}
+function palindrome2(str) {
+  const re = /[\W_]/g;
+  const strToLower = str.toLowerCase().replace(re, '');
+  const revered = strToLower
+    .split('')
+    .reverse()
+    .join('');
+  return revered === str;
+}
+function palindrome3(str) {
+  const re = /[^A-Za-z0-9]/g;
+  str = str.toLowerCase().replace(re, '');
+  const len = str.length;
+  for (let i = 0; i < len / 2; i++) {
+    if (str[i] !== str[len - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+}
 
 module.exports = palindrome;
