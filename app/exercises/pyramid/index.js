ec0979c5ad498805b0ccce73507b68f2b19b9dcb
marcell <ciszekmarcell@gmail.com>
2019-04-17 08:27:44 +0200

    recursion pyramid

--- app/exercises/pyramid/index.js
+++ app/exercises/pyramid/index.js
@@ -32,7 +32,22 @@
 // }
 
 function pyramid(n, row = 0, level = '') {
-  //
+  if (row === n) {
+    return;
+  }
+  if (level.length === 2 * n - 1) {
+    console.log(level);
+    // next row!
+    return pyramid(n, row + 1);
+  }
+  const middle = Math.floor((2 * n - 1) / 2);
+  let add;
+  if (middle - row <= level.length && middle + row >= level.length) {
+    add = '#';
+  } else {
+    add = ' ';
+  }
+  pyramid(n, row, level + add);
 }
 
 console.log(pyramid(5));
