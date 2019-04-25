17d2874562806c9c333a3d04da2a25b0d111fafb
marcell <ciszekmarcell@gmail.com>
2019-04-25 10:05:44 +0200

    stack and queaues

--- app/exercises/qfroms/index.js
+++ app/exercises/qfroms/index.js
@@ -4,6 +4,7 @@
 // Queue should implement the methods 'add', 'remove', and 'peek'.
 // For a reminder on what each method does, look back
 // at the Queue exercise.
+//
 // --- Examples
 //     const q = new Queue();
 //     q.add(1);
@@ -14,6 +15,38 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.first = new Stack();
+    this.second = new Stack();
+  }
+
+  add(record) {
+    this.first.push(record);
+  }
+
+  remove() {
+    while (this.first.peek()) {
+      this.second.push(this.first.pop());
+    }
+    const record = this.second.pop();
+    while (this.second.peek()) {
+      this.first.push(this.second.pop());
+    }
+    return record;
+  }
+
+  peek() {
+    while (this.first.peek()) {
+      this.second.push(this.first.pop());
+    }
+    const record = this.second.peek();
+
+    while (this.second.peek()) {
+      this.first.push(this.second.pop());
+    }
+    return record;
+  }
+}
 
 module.exports = Queue;
