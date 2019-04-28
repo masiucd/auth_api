47683b50d8280fdc94c8e0eabb7357c865f92417
marcell <ciszekmarcell@gmail.com>
2019-04-28 10:06:15 +0200

    linked list insert and size

--- app/exercises/linkedlist/index.js
+++ app/exercises/linkedlist/index.js
@@ -2,8 +2,37 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+  constructor(data, next) {
+    this.data = data;
+    this.next = next;
+  }
+}
 
-class LinkedList {}
+class LinkedList {
+  constructor(head) {
+    this.head = null;
+  }
+
+  insertFirst(data) {
+    this.head = new Node(data, this.head);
+  }
+
+  size() {
+    let counter = 0;
+    let node = this.head;
+    while (node) {
+      counter++;
+      node = node.next;
+    }
+    return counter;
+  }
+}
+
+const myLinkedList = new LinkedList();
+myLinkedList.head = new Node(4);
+myLinkedList.insertFirst(7);
+myLinkedList.insertFirst(12);
+console.log(myLinkedList.size());
 
 module.exports = { Node, LinkedList };
