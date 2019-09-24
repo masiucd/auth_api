b19fb89b492e50f43fc297986c16b5e7551e62ff
Masiu <ciszekmarcell@gmail.com>
2019-09-24 11:27:33 +0200

    bst

--- app/exercises/bst/index.js
+++ app/exercises/bst/index.js
@@ -11,6 +11,29 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(data) {
+    // using recursion
+    // handle left
+    if (data < this.data && this.left) {
+      this.left.insert(data);
+      // if no node on the left side
+    } else if (data < this.data) {
+      this.left = new Node(data);
+      // handle right
+    } else if (data > this.data && this.right) {
+      this.right.insert(data);
+    } else if (data > this.data) {
+      // if no node on the right side
+      this.right = new Node(data);
+    }
+  }
+}
 
 module.exports = Node;
