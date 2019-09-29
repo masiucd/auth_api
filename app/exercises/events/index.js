79885fe188da1ea8c626f45508b6e23e7b83779c
Masiu <ciszekmarcell@gmail.com>
2019-09-29 10:23:54 +0200

    js events

--- app/exercises/events/index.js
+++ app/exercises/events/index.js
@@ -1,19 +1,38 @@
+/* eslint-disable no-undef */
 // --- Directions
 // Create an 'eventing' library out of the
 // Events class.  The Events class should
 // have methods 'on', 'trigger', and 'off'.
 
 class Events {
+  constructor() {
+    this.events = {};
+  }
+
   // Register an event handler
-  on(eventName, callback) {}
+  on(eventName, callback) {
+    if (this.events[eventName]) {
+      this.events[eventName].push(callback);
+    } else {
+      this.events[eventName] = [callback];
+    }
+  }
 
   // Trigger all callbacks associated
   // with a given eventName
-  trigger(eventName) {}
+  trigger(eventName) {
+    if (this.events[eventName]) {
+      for (const cb of this.events[eventName]) {
+        cb();
+      }
+    }
+  }
 
   // Remove all event handlers associated
   // with the given eventName
-  off(eventName) {}
+  off(eventName) {
+    delete this.events[eventName];
+  }
 }
 
 module.exports = Events;
