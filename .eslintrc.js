037f99b51da076eb4ade7e13c5087ef14604ae86
Marcell Ciszek <ciszekmarcell@gmail.com>
2022-04-24 17:22:47 +0200

    Set up server

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,35 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: "latest", // Allows the use of modern ECMAScript features
-    sourceType: "module", // Allows for the use of imports
+    tsconfigRootDir: __dirname,
+    project: "./tsconfig.json",
+    sourceType: "module",
   },
-  extends: ["plugin:@typescript-eslint/recommended"], // Uses the linting rules from @typescript-eslint/eslint-plugin
-  env: {
-    node: true, // Enable Node.js global variables
-  },
+  plugins: ["@typescript-eslint", "prettier"],
+  extends: [
+    "eslint:recommended",
+    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/src/configs/recommended.json
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier",
+  ],
   rules: {
-    "no-console": "off",
-    "import/prefer-default-export": "off",
-    "@typescript-eslint/no-unused-vars": "warn",
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "all",
+        argsIgnorePattern: "^_",
+        ignoreRestSiblings: true,
+      },
+    ],
+    "@typescript-eslint/explicit-member-accessibility": [
+      "off",
+      {
+        accessibility: "explicit",
+      },
+    ],
   },
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,37 @@
-function sayMyName(name: string): void {
-  if (name === "Heisenberg") {
-    console.log("You're right 👍")
-  } else {
-    console.log("You're wrong 👎")
-  }
+import Fastify, {FastifyInstance, RouteShorthandOptions} from "fastify"
+import {Server, IncomingMessage, ServerResponse} from "http"
+
+const server: FastifyInstance = Fastify({})
+
+const opts: RouteShorthandOptions = {
+  schema: {
+    response: {
+      200: {
+        type: "object",
+        properties: {
+          pong: {
+            type: "string",
+          },
+        },
+      },
+    },
+  },
 }
 
-sayMyName("Heisenberg")
+server.get("/ping", opts, async (request, reply) => {
+  return {pong: "it worked!"}
+})
+
+const start = async () => {
+  try {
+    await server.listen(3000)
+
+    const address = server.server.address()
+    const port = typeof address === "string" ? address : address?.port
+    console.log(`server listening on ${port} 🚀`)
+  } catch (err) {
+    server.log.error(err)
+    process.exit(1)
+  }
+}
+start()
